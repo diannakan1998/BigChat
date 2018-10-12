43ab0f95bd622a12534eaa0adf9682d546b5eccb
diannakan1998 <diannakan1998@gmail.com>
2018-10-12 15:25:36 -0700

    init

--- BigChat/BigChat/urls.py
+++ BigChat/BigChat/urls.py
@@ -14,13 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import path
 
 urlpatterns = [
-    
     path('admin/', admin.site.urls),
-    path(r'^auth/', include('rest_framework_social_oauth2.urls')),
-    # path('auth/', include('auth.urls')),
-    # path('accounts/', include('allauth.urls')),
-    
 ]
