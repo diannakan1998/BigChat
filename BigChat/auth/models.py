6bcf0974c25060958c4d9e0ea4addc4c8410422d
Harminder Kandola <harminderk@hotmail.ca>
2018-10-21 17:39:33 -0700

    adding..

--- BigChat/auth/models.py
+++ BigChat/auth/models.py
@@ -5,28 +5,29 @@ from django.contrib.postgres.fields import ArrayField
 # Create your models here.
 
 
-# class Users(models.Model):
-#      email = models.CharField( max_length=100, validators=[ EmailValidator() ], blank=False, unique=True)
-#      user_id = models.AutoField(primary_key=True)
-#      token = models.CharField(max_length=255, unique=True, blank=False)
-#      chat_list_id = models.TextField(blank=True)
+class Users(models.Model):
+     email = models.CharField( max_length=100, validators=[ EmailValidator() ], blank=False, unique=True)
+     user_id = models.AutoField(primary_key=True)
+     token = models.CharField(max_length=255, unique=True, blank=False)
+     chat_list_id = models.TextField(default="")
 
-#      class Meta:
-#          db_table = 'Users'
-
-
+     class Meta:
+         db_table = 'myAuth_users'
 
 
-def getChatListModel(db_table):
+
+
+
+def getChatListModel(name):
  class ChatList(models.Model):
-     chat_id = models.TextField()
-     last_message = models.TextField()
-     message_type = models.IntegerField()
-     read_flag = models.IntegerField()
-     date_modified = models.DateTimeField()
+     chat_id = models.TextField(default="")
+     last_message = models.TextField(default="")
+     message_type = models.IntegerField(blank=True)
+     read_flag = models.IntegerField(blank=True)
+     date_modified = models.DateTimeField(default="")
      
      class Meta:
-         db_table = db_table
+         db_table = name
 
  return ChatList
 
